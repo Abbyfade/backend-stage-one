5e544feb92418bc4fa61148a967fe8b9e8e54d65
User <Test@FADEYI-PC.lasuth.org.ng.local>
2022-10-28 12:37:56 +0100

    changes

--- detail/views.py
+++ detail/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
-
-def index(request):
+def index(request, *args, **kwargs):
+    header = {"Access-Control-Allow-Origin":"*"}
     responseData = {
         "slackUsername": "abby10", 
         "backend": True, 
         "age": 21,
         "bio": "Hi there! I'm Abiola, a medical student and a tech enthusiast, i'm in the backend track of hng9"
     }
-    return JsonResponse(responseData)
+    return JsonResponse(responseData, headers=header)
